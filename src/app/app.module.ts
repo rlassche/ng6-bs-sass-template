aa676cd0f7970b244ed7fd4402026d431b01cf02
Your Name <you@example.com>
2018-08-27 10:19:14 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { AppRoutingModule } from './app-routing.module';
+
+//import { RouterModule} from '@angular/router';
+import { routes} from './routes';
+
+//import { TestModule } from './test/test.module';
+
 import { AppComponent } from './app.component';
+import { NocontentComponent } from './nocontent/nocontent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NocontentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
